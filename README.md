f079dd59e492895caab7f72a660d018204e3cedd
amiralisobhgol <amiralisobhgol@cafebazaar.ir>
2020-08-04 20:49:02 +0430

    add minor details to resume

---
+++ README.md
@@ -0,0 +1,13 @@
+### Installation
+
+- Install [ resume-cli ](https://github.com/jsonresume/resume-cli)
+
+- Run:
+`
+resume export --theme=kendall-custom ./build/tmp.html
+`
+- Then for the PDF output, run:
+`
+node export-pdf.js
+`
+you can see the pdf in the build directory
--- export-pdf.js
+++ export-pdf.js
@@ -7,7 +7,7 @@ async function printPDF() {
   await page.emulateMediaType('screen');
   const pdf = await page.pdf({ 
     width: '10.9in',
-    height: '13.0in',
+    height: '13.5in',
     path: './build/resume.pdf',
     printBackground: true,
     displayHeaderFooter: true,
